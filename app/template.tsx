a86b1f7d59b86d296de40dc4bc6bfe8cc9e768f3
moccet <sofian@moccet.com>
2025-12-05 16:24:25 +0000

    new routing

---
+++ app/template.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { motion, AnimatePresence } from 'framer-motion';
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          duration: 0.3,
+          ease: 'easeInOut'
+        }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
